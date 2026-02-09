cde9193524bbad4d9b7d56e60ff1bcd016635b5f
Shahzada Shoaib <shahzada.shoaib011@gmail.com>
2026-02-10 03:01:42 +0500

    revert changes

--- next.config.ts
+++ next.config.ts
@@ -15,21 +15,6 @@ const nextConfig: NextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
   },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.mustafazahid.com',
-          },
-        ],
-        destination: 'https://mustafazahid.com/:path*',
-        permanent: true,
-      },
-    ];
-  },
   async headers() {
     return [
       {
