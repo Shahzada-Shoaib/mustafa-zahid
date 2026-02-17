3c961471f53215767623d925caec21df3cdc6fe9
Shahzada Shoaib <shahzada.shoaib011@gmail.com>
2026-02-17 22:36:56 +0500

    deployment issue

--- components/dashboard/RichTextEditor.tsx
+++ components/dashboard/RichTextEditor.tsx
@@ -46,7 +46,7 @@ const RichTextEditor = forwardRef<RichTextEditorRef, RichTextEditorProps>(
     // Update editor content when value prop changes
     useEffect(() => {
       if (editor && value !== editor.getHTML()) {
-        editor.commands.setContent(value, false);
+        editor.commands.setContent(value, { emitUpdate: false });
       }
     }, [value, editor]);
 
